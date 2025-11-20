36b90c2d08e4400fe5f3704be7ac225a22238122
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-11-19 21:21:46 -0300

    refactor: change function name in MovieMetaValue

--- src/entities/Movie/ui/MovieMeta/MovieMetaValue.jsx
+++ src/entities/Movie/ui/MovieMeta/MovieMetaValue.jsx
@@ -1,6 +1,6 @@
 import { cn } from "@/shared/lib/utils";
 
-export function MovieMetaLabel({ children, className }) {
+export function MovieMetaValue({ children, className }) {
   return (
     <p className={cn("font-normal text-[18px] text-[#7a7a7a]", className)}>
       {children}
