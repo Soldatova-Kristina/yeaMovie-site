a71febcbd5612588b6bc08f99944784770c30db2
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-11-25 16:44:51 -0300

    feat: change function name from SearchMovies to SearchMovie, change path way using @

--- src/pages/SearchResultsPage/ui/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage/ui/SearchResultsPage.jsx
@@ -1,12 +1,12 @@
 import { useSearchParams } from 'react-router-dom';
-import { useSearchMovies } from '@/features/SearchMovie/model/useSearchMovie';
+import { useSearchMovie } from '@/features/SearchMovie/model/useSearchMovie';
 import { MoviesGrid } from '@/widgets/MoviesGrid';
 import { ErrorAndLoadingSection } from '@/shared/ui/PageStatus';
 
 export default function SearchResultsPage() {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query');
-  const { movies, loading, error } = useSearchMovies(query);
+  const { movies, loading, error } = useSearchMovie(query);
   return (
     <div className="min-h-screen bg-white">
       <div className="container-custom py-[80px]">
